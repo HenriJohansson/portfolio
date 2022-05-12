34a066a849e6c7528034952b9683b3957450b3de
Henri Johansson <henri.mm.johansson@gmail.com>
2022-05-12 16:53:03 +0300

    Working Downloadlink from GoogleDrive

---
+++ src/components/header/CallToAction.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const CallToAction = () => {
+  return (
+    <div className='cta'>
+        <a download href='https://drive.google.com/uc?export=download&id=1ArGv2JwTGnjqFwg9f3iltPBzJdRx9z5Y'>
+            Download CV</a>
+        <a href='#contact'>Let's talk</a>
+    </div>
+  )
+}
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './header.css'
+import { CallToAction } from './CallToAction'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Henri Johansson</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CallToAction/>
+      </div>
+    </header>
   )
 }
