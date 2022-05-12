763ae7e15f2a4ee3ffc4a5ddada2933cf9fb9511
Henri Johansson <henri.mm.johansson@gmail.com>
2022-05-13 00:33:17 +0300

    Fixing css, added my Transparent pic, 2 buttons.

--- src/components/header/CallToAction.jsx
+++ src/components/header/CallToAction.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const CallToAction = () => {
   return (
     <div className='cta'>
-        <a download href='https://drive.google.com/uc?export=download&id=1ArGv2JwTGnjqFwg9f3iltPBzJdRx9z5Y'>
+        <a download className='btn' href='https://drive.google.com/uc?export=download&id=1ArGv2JwTGnjqFwg9f3iltPBzJdRx9z5Y'>
             Download CV</a>
-        <a href='#contact'>Let's talk</a>
+        <a href='#contact' className='btn btn-primary'>Let's talk</a>
     </div>
   )
 }
